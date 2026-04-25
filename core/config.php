9c84524005e04bf5564dae5932b6015c3d8c3d1d
Zuazuito <marthahdezmartinez@gmail.com>
2026-04-24 21:01:43 -0600

    add(core):Se agrego carpeta de conexion

---
+++ core/config.php
@@ -0,0 +1,36 @@
+<?php
+
+return [
+
+    //* Liz
+    'db' => [
+        'conexion_Mundiales' => [
+            'host' => 'localhost',
+            'dbname' => 'nlgo',
+            'username' => 'root',
+            'password' => 'Noe_050703',
+            'charset' => 'utf8mb4',   //Es para los caracteres especiales
+        ]
+    ],
+
+     //*aby
+     'db1' => [
+        'conexion_Mundiales' => [
+            'host' => 'localhost',
+            'dbname' => 'pw2_db_main',
+            'username' => 'root',
+            'password' => 'root',
+            'charset' => 'utf8mb4',   //Es para los caracteres especiales
+        ]
+    ] ,
+     //*andreiy
+   'db2' => [
+        'conexion_Mundiales' => [
+            'host' => 'localhost',
+            'dbname' => 'pw2_db_main',
+            'username' => 'root',
+            'password' => 'root',
+            'charset' => 'utf8mb4',   //Es para los caracteres especiales
+        ]
+    ] 
+];
