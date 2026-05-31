f7240b435a4c2320699bd2270299d3285c4015cb
Zuazuito <marthahdezmartinez@gmail.com>
2026-05-31 15:09:46 -0600

    feat(Routes/rutas.js)Agregue las rutas de las paginas al servidor

--- routes/Rutas.js
+++ routes/Rutas.js
@@ -34,14 +34,7 @@ router.get('/' ,
 
 // ruta Universal, aqui para cuando pongas "localhost:(puerto)" aparezca el .html que le pongas aqui
 
-router.get('/login', (req, res) => {
-    res.sendFile('Html/login.html', { root: './public' })
-})
-
-router.get('/register', (req, res) => {
-    res.sendFile('Html/register.html', { root: './public' })
-})
-
+//*Header
 router.get('/header', (req, res) => {
     res.sendFile('Html/partial/header.html', { root: './public' })
 })
@@ -50,6 +43,51 @@ router.get('/header-login', (req, res) => {
     res.sendFile('Html/partial/header-login.html', { root: './public' })
 })
 
+//*usuarios
+
+router.get('/login', (req, res) => {
+    res.sendFile('Html/login.html', { root: './public' })
+})
+
+router.get('/register', (req, res) => {
+    res.sendFile('Html/register.html', { root: './public' })
+})
+
+router.get('/profile', (req, res) => {
+    res.sendFile('Html/profile.html', { root: './public' })
+})
+
+router.get('/editProfile', (req, res) => {
+    res.sendFile('Html/editProfile.html', { root: './public' })
+})
+
+//*CHAT */
+router.get('/chatAdd', (req, res) => {
+    res.sendFile('Html/chatAdd.html', { root: './public' })
+})
+
+router.get('/ChatNotification', (req, res) => {
+    res.sendFile('Html/ChatNotification.html', { root: './public' })
+})
+
+router.get('/messageChat', (req, res) => {
+    res.sendFile('Html/messageChat.html', { root: './public' })
+})
+
+//*COUPON
+router.get('/coupon', (req, res) => {
+    res.sendFile('Html/coupon.html', { root: './public' })
+})
+
+//*EVENTS
+router.get('/events', (req, res) => {
+    res.sendFile('Html/events.html', { root: './public' })
+})
+
+//*PLACE
+router.get('/place', (req, res) => {
+    res.sendFile('Html/place.html', { root: './public' })
+})
 
 
 
