0c002df78c37cb6cd70e002bb221f8803ab8ea88
Zuazuito <marthahdezmartinez@gmail.com>
2026-06-03 19:12:00 -0600

    register(server.js,register.js)
    -Tiene error en la imagen

--- public/js/register.js
+++ public/js/register.js
@@ -11,3 +11,108 @@ fileInput.addEventListener('change', (event) => {
         reader.readAsDataURL(file);
     }
 });
+
+document.getElementById('togglePassword').addEventListener('click' , function(){
+
+    const input = document.getElementById('idPassword');
+    const icon = document.getElementById('eyeIcon');
+
+    if (input.type === 'password') {
+        input.type = 'text';
+        icon.classList.remove('bi-eye-fill');
+        icon.classList.add('bi-eye-slash-fill');
+    }else{
+        input.type = 'password';
+        icon.classList.remove('bi-eye-slash-fill');
+        icon.classList.add('bi-eye-fill');
+    }
+});
+
+const formData = new FormData();
+document.getElementById('formRegister').addEventListener('submit', async (e) => {
+
+    e.preventDefault();
+
+    formData.append('mail',document.getElementById('idMail').value);
+    formData.append('nickname',document.getElementById('idNickname').value);
+    formData.append('nombre',document.getElementById('idNombre').value) ;
+    formData.append('date',document.getElementById('idDate').value);
+    formData.append('password' ,document.getElementById('idPassword').value)
+
+    const fileInput = document.getElementById('fileOpeneReg');
+    if (fileInput.files[0]) {
+        formData.append('fileOpeneReg',fileInput.files[0]);
+    }
+
+    fetch('/feature-register', {
+
+        method: 'POST',
+        body: formData
+
+       
+    })
+        .then(response => response.json())
+        .then(data => {
+
+            /*  console.log(JSON.stringify(data, null, 2)); */
+              
+             
+            if (data.msg === "Este usuario ya existe") {
+                 Swal.fire({
+                    icon: "error",
+                    text: data.msg,
+                    customClass: {
+                        text: 'letter',
+                        confirmButton: 'btn-ok',
+                        icon: 'icon-custom'
+                    },
+                    confirmButtonText: 'Aceptar',
+                    confirmButtonColor: '#1D2984',
+                    iconColor: '#1D2984'
+                });
+            } else if(data.msg==="Usuario registrado") {
+
+                 Swal.fire({
+                    icon: "success",
+                    text: data.msg,
+                    customClass: {
+                        text: 'letter',
+                        confirmButton: 'btn-ok',
+                        icon: 'icon-custom'
+                    },
+                    confirmButtonText: 'Aceptar',
+                    confirmButtonColor: '#1D2984',
+                    iconColor: '#1D2984'
+                }).then(result =>{
+
+                    if(result.isConfirmed){
+                        window.location.href = data.redirect;
+                    }
+                });
+                   
+
+            }else{
+                 Swal.fire({
+                    icon: "error",
+                    text: data.msg,
+                    customClass: {
+                        text: 'letter',
+                        confirmButton: 'btn-ok',
+                        icon: 'icon-custom'
+                    },
+                    confirmButtonText: 'Aceptar',
+                    confirmButtonColor: '#1D2984',
+                    iconColor: '#1D2984'
+                });
+            }
+
+        })
+        .catch(error => {
+
+            console.error("Error en el fetch ", error);
+            
+        })
+
+
+});
+
--- server.js
+++ server.js
@@ -2,13 +2,12 @@
 const express = require('express');
 const mysql = require('mysql2');
 const multer = require('multer');
-const path=require('path');
+const path = require('path');
 const app = express();
 
 const PagesRoutes = require('./routes/Rutas.js'); //Variable que guarda la direccion de mis rutas
 
 const PORT = 3001;
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -16,7 +15,7 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 //Importar las rutas personalizadas
-app.use('/',PagesRoutes); 
+app.use('/', PagesRoutes);
 
 // Escuchar servidor
 app.listen(PORT, () => {
@@ -36,47 +35,135 @@ const Db = mysql.createConnection({
 Db.connect((ErrorConexion) => {
 
   if (ErrorConexion) {
-      console.log(ErrorConexion);
-      return;
+    console.log(ErrorConexion);
+    return;
   } else {
-      console.log('Conectado a la base de datos');
+    console.log('Conectado a la base de datos');
   }
 });
 
+const strg = multer.memoryStorage();
+const archivo = multer({
+    storage: strg
+
+}
+)
 
 //Iniciar sesion
 
 app.post('/feature-login', async (req, res) => {
-  
-  const { mailUser, passwordUser} = req.body; //Estos son los name de los input
 
- // Validar que los campos no estén vacíos
+  const { mailUser, passwordUser } = req.body; //Estos son los name de los input
+
+  // Validar que los campos no estén vacíos
   if (!mailUser || !passwordUser) {
     return res.status(400).json({ msg: 'Todos los campos son obligatorios' });
   }
 
   Db.query('call sp_userLogin(?,?,@existe,@mensaje)', [mailUser, passwordUser], (err, result) => {
-      if (err) {
-          console.log(err);
-          res.json({ msg: "Error" });
-      } 
+    if (err) {
+      console.log(err);
+      res.json({ msg: "Error" });
+    }
 
-      Db.query('select @existe as existe , @mensaje as mensaje', (err2,result2)=>{
+    Db.query('select @existe as existe , @mensaje as mensaje', (err2, result2) => {
 
-        const {existe,mensaje} = result2[0];
-        if (existe === 1) {
-          res.json({
-            msg:mensaje,
-            info: result[0][0],
-            redirect:"/place"
-          });
+      const { existe, mensaje } = result2[0];
+      if (existe === 1) {
+        res.json({
+          msg: mensaje,
+          info: result[0][0],
+          redirect: "/place"
+        });
 
-        } else {
-          res.json({ msg:mensaje});
-        }
-      })
+      } else {
+        res.json({ msg: mensaje });
+      }
+    })
   });
 
 });
 
 
+
+//Registrar usuario
+
+app.post('/feature-register',archivo.single('fileOpeneReg'), async (req, res) => {
+
+  if (!req.file) {
+   return res.status(400).json({msg: 'La foto es obligatoria'});
+  }
+
+  const { mail, nickname, nombre, date, password } = req.body;  //Estos son los name de los input
+  const imagen = req.file.buffer.toString('base64');
+
+ 
+  // Validar que los campos no estén vacíos
+  if (!mail || !nickname || !nombre || !date || !password ) {
+    return res.status(400).json({ msg: 'Todos los campos son obligatorios' });
+  }
+
+
+  const checkEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!checkEmail.test(mail)) {
+    return res.status(400).json({ msg: 'El correo no es válido' });
+  }
+
+
+  const checkPalabras = /^[A-Za-zÁÉÍÓÚáéíóúÑñ\s]+$/;
+
+  if (!checkPalabras.test(nombre)) {
+    return res.status(400).json({ msg: 'El nombre solo puede contener letras y espacios' });
+  }
+
+  if (!checkPalabras.test(nickname)) {
+    return res.status(400).json({ msg: 'El nickname solo puede contener letras y espacios' });
+  }
+
+  const fechaNacimiento = new Date(date); //
+
+  if (isNaN(fechaNacimiento.getTime())) {
+    return res.status(400).json({ msg: 'La fecha de nacimiento no es válida' });
+  }
+
+  const hoy = new Date();
+  if (fechaNacimiento > hoy) {
+    return res.status(400).json({ msg: 'La fecha de nacimiento no puede ser en el futuro' });
+  }
+
+  // Validar contraseña
+  const checkContra = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\w\s]).{8,}$/;
+  if (!checkContra.test(password)) {
+    return res.status(400).json({ msg: 'La contraseña no cumple con los requisitos mínimos' });
+  }
+
+  
+
+  Db.query('call sp_userRegister(?,?,?,?,?,? , @existe, @mensaje)', [mail, nickname, nombre, date, password,imagen], (err, result) => {
+    if (err) {
+      console.log(err);
+      return res.status(500).json({ msg: "Error en el servidor" });
+    }
+
+    Db.query('select @existe as existe, @mensaje as mensaje', (err2, result2) => {
+      if (err2) {
+        console.log(err2);
+        return res.status(500).json({ msg: "Error en el servidor" });
+      }
+
+      const { existe, mensaje } = result2[0];
+      if (existe === 1) {
+        console.log(mensaje)
+         res.json({ msg: mensaje });
+
+      } else {
+         res.json({
+          msg: mensaje,
+          redirect: "/login"
+        });
+      }
+    });
+  }
+  );
+
+})
