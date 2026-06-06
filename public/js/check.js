54a02ff1b6c8328e0d805b51e4ad113a7b9c180a
Andreiy <112518884+DreyAndreiy@users.noreply.github.com>
2026-06-05 18:51:20 -0600

    Redirección a register.html
    Usar check.js apenas inicie el body para confirmar y utilizarlo en otros mas

---
+++ public/js/check.js
@@ -0,0 +1,8 @@
+const usuario = localStorage.getItem('usuario');
+    /*console.log(usuario)*/
+
+
+if(!localStorage.getItem('usuario')){
+    /*console.log("No hay sesion");*/
+    window.location.href = '/Html/register.html';
+}
